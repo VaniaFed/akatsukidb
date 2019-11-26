a6a65a2e98e8fe10fe8827ef49ecf5ffeb910a51
VaniaFed <ztankist@gmail.com>
2019-11-26 16:02:43 +0300

    refactor(createTable): create few functions for make the code more descriptive

--- src/actions/create-table.js
+++ src/actions/create-table.js
@@ -11,23 +11,32 @@ export const createTable = (queryString, databaseId) => {
 };
 
 const getTableData = queryString => {
-    const regx = /create\stable\s(\w+)\s\(([\w\s\d,()"']+)\)/i;
-    const tableName = queryString.match(regx)[1];
-    const tableDataString =  queryString.match(regx)[2];
-    const tableData = tableDataString.split(/,\s/i).map(item => {
-        const column = item.split(/\s/i);
-        const name = column[0],
-              type = column[1];
-        return ({
-            name,
-            type
-        });
-    });
-    return ({
+    const rx = /create\stable\s(\w+)\s\(([\w\s\d,()"']+)\)/i;
+    const tableName = extractTableName(queryString, rx);
+    const tableData = extractTableData(queryString, rx);
+    const columns = convertColumns(tableData);
+
+    return {
         name: tableName,
         structure: {
-            columns: tableData
+            columns
         }
-    });
+    };
 };
 
+const extractTableName = (s, rx) => {
+    return s.match(rx)[1];
+};
+
+const extractTableData = (queryString, rx) => {
+    return queryString.match(rx)[2];
+};
+
+const convertColumns = (tableData) => {
+    const columns = tableData.split(/,\s/i);
+    return columns.map(item => {
+        const [name, type] = item.split(/\s/i);
+
+        return { name, type };
+    });
+};
